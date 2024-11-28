173c526f38af1b6d5765960d5a1cb50a0ae6fb1b
Alan_Luo <a8576954111@gmail.com>
2024-11-28 21:48:11 +0800

    first

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ['picsum.photos'],
+  },
+}
+
+module.exports = nextConfig
